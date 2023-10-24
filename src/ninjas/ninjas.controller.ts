7b44b13d72f075528c06a5bc2ee6d92501330438
Jason <jasonkwm13@gmail.com>
2023-10-24 12:30:44 +0800

    feat: add users route using 'nest g resource users' and update controller and dto for ninjas

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,29 +1,45 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateNinjasDto } from './dto/create-ninjas.dto';
 @Controller('ninjas')
 export class NinjasController {
+  // GET /ninjas?type=fast --> [...]
   @Get()
-  getNinjas() {
-    return [];
+  getNinjas(@Query('type') type: string) {
+    return [{ type }];
   }
 
+  // GET /ninjas/:id --> {...}
   @Get(':id')
   getOneNinjas(@Param('id') id: string) {
     return { id };
   }
 
+  // POST /ninjas
   @Post()
-  postNinjas() {
-    return {};
+  postNinjas(@Body() body: CreateNinjasDto) {
+    return {
+      name: body.name,
+    };
   }
 
-  @Put()
-  putNinjas() {
-    return {};
+  // PUT /ninjas/:id --> {...}
+  @Put(':id')
+  putNinjas(@Param('id') id: string) {
+    return { id };
   }
 
-  @Delete()
-  deleteNinjas() {
-    return {};
+  // DELETE /ninjas/:id --> {...}
+  @Delete(':id')
+  deleteNinjas(@Param('id') id: string) {
+    return { id };
   }
 }
